d10273b2c5c60c3d2c0643ea5aed1c27ae240b29
StephanSeifert <37901395+StephanSeifert@users.noreply.github.com>
2018-09-05 14:05:16 +0200

    Add files via upload

--- R/addSurrogates.R
+++ R/addSurrogates.R
@@ -8,6 +8,7 @@
 #' @param trees list of trees created by getTreeranger.
 #' @param s Predefined number of surrogate splits (it may happen that the actual number of surrogate splits differes in individual nodes). Default is 1 \% of no. of variables.
 #' @param Xdata data without the dependent variable.
+#' @param num.threads number of threads used for parallel execution. Default is number of CPUs available.
 #' @return a list with trees containing of lists of nodes with the elements:
 #' \itemize{
 #' \item nodeID: ID of the respective node (important for left and right daughters in the next columns)
@@ -22,27 +23,21 @@
 #' }
 #' @export
 
-addSurrogates=function(RF,trees,s,Xdata){
+addSurrogates = function(RF,trees,s,Xdata,num.threads) {
 
   if (exists("s")) {
-    s=s
+    s = s
   } else {
-    s=nvar*0.01
+    s = nvar*0.01
   }
-  nvar=ncol(Xdata)            # count number of variables
-  ntree= length(trees)
-  # categories for variables 0 for continuous (has to be defined for surrogate variables searching)
-  ncat = rep(0, nvar)
+  ntree = length(trees)
   #variables to find surrogates (control file similar as in rpart)
-  controls = list(minsplit = 0L, minbucket = 0L, cp = 0L,
-                  maxcompete = 0L, maxsurrogate = as.integer(s), usesurrogate = 0L,
-                  surrogatestyle = 0L, maxdepth = 0L, xval = 0L)
+  controls = list(maxsurrogate = as.integer(s), sur_agree = 0, nthreads = num.threads)
 
-  trees.surr=lapply(1:ntree,getSurrogate,maxsurr=s,surr.par=list(ncat = ncat,
-                                                                inbag.counts=RF$inbag.counts,
-                                                                Xdata=Xdata,
-                                                                controls=controls,
-                                                                trees=trees))
+  trees.surr = lapply(1:ntree, getSurrogate, maxsurr = s, surr.par = list(inbag.counts = RF$inbag.counts,
+                                                                Xdata = Xdata,
+                                                                controls = controls,
+                                                                trees = trees))
   return(trees.surr)
 }
 
@@ -51,13 +46,13 @@ addSurrogates=function(RF,trees,s,Xdata){
 #' This is an internal function
 #'
 #' @keywords internal
-getSurrogate=function(surr.par,k=1,maxsurr){
+getSurrogate = function(surr.par, k = 1, maxsurr) {
   #weights and trees are extracted
- tree=surr.par$trees[[k]]
- column.names=colnames(tree)
- n.nodes=nrow(tree)
- wt=surr.par$inbag.counts[[k]]
- tree.surr=lapply(1:n.nodes,SurrTree,ncat=surr.par$ncat,wt=wt,Xdata=surr.par$Xdata,controls=surr.par$controls,column.names,tree,maxsurr)
+ tree = surr.par$trees[[k]]
+ column.names = colnames(tree)
+ n.nodes = nrow(tree)
+ wt = surr.par$inbag.counts[[k]]
+ tree.surr = lapply(1:n.nodes,SurrTree,wt = wt,Xdata = surr.par$Xdata,controls = surr.par$controls, column.names, tree,maxsurr)
 }
 
 #' SurrTree
@@ -65,35 +60,34 @@ getSurrogate=function(surr.par,k=1,maxsurr){
 #' This is an internal function
 #'
 #' @keywords internal
-SurrTree=function(j,ncat,wt,Xdata,controls,column.names,tree,maxsurr){
-  node=tree[j,]
+SurrTree = function(j,wt,Xdata,controls,column.names,tree,maxsurr) {
+  node = tree[j,]
   # for non-terminal nodes get surrogates
-  if (node["status"]==1){
-  pnode=node[4:5]
+  if (node["status"] == 1) {
+  pnode = node[4:5]
   #Handover to C
-  surrogate.parameters= .Call(C_getSurrogates,
-                              ncat = as.integer(ncat),
-                              wt=as.numeric(wt),
-                              X=as.matrix(Xdata),
-                              controls=as.integer(unlist(controls)),
+  surrogate.parameters = .Call(C_getSurrogates,
+                              wt = as.numeric(wt),
+                              X = as.matrix(Xdata),
+                              controls = as.integer(unlist(controls)),
                               as.numeric(pnode))
-  if (nrow(surrogate.parameters$isplit)>1){
-    surrogates=surrogate.parameters$isplit[2:nrow(surrogate.parameters$isplit),1]
-    surr.adj=surrogate.parameters$dsplit[2:nrow(surrogate.parameters$dsplit),3]
-    node.new=c(node,surrogates,surr.adj)
-    surrogate.names=NULL
-    adj.names=NULL
-    surrogate.names=sapply(1:length(surrogates),name.surr,surrogate.names)
-    adj.names=sapply(1:length(surrogates),name.adj,adj.names)
-    names(node.new)=c(column.names,surrogate.names,adj.names)
+  if (nrow(surrogate.parameters$isplit) > 1) {
+    surrogates = surrogate.parameters$isplit[2:nrow(surrogate.parameters$isplit),1]
+    surr.adj = surrogate.parameters$dsplit[2:nrow(surrogate.parameters$dsplit),3]
+    node.new = c(node,surrogates,surr.adj)
+    surrogate.names = NULL
+    adj.names = NULL
+    surrogate.names = sapply(1:length(surrogates),name.surr,surrogate.names)
+    adj.names = sapply(1:length(surrogates),name.adj,adj.names)
+    names(node.new) = c(column.names,surrogate.names,adj.names)
   }
 
-  if (nrow(surrogate.parameters$isplit)==1){
-    node.new=node
+  if (nrow(surrogate.parameters$isplit) == 1) {
+    node.new = node
   }
   }
-  if (node["status"]==0){
-    node.new=node
+  if (node["status"] == 0) {
+    node.new = node
   }
 return(node.new)
 }
@@ -103,8 +97,8 @@ return(node.new)
 #' This is an internal function
 #'
 #' @keywords internal
-name.surr=function(i,surrogate.names){
-surrogate.names=c(surrogate.names,paste0("surrogate_",i))
+name.surr = function(i,surrogate.names){
+surrogate.names = c(surrogate.names,paste0("surrogate_",i))
 return(surrogate.names)
 }
 
@@ -113,8 +107,8 @@ return(surrogate.names)
 #' This is an internal function
 #'
 #' @keywords internal
-name.adj=function(i,adj.names){
-  adj.names=c(adj.names,paste0("adj_",i))
+name.adj = function(i,adj.names){
+  adj.names = c(adj.names,paste0("adj_",i))
   return(adj.names)
 }
 
