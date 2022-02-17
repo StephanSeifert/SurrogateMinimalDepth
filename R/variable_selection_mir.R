d2ef93d9f150bfc881977c70d7e8b513b5a139b6
Stephan Seifert <stephan.seifert@chemie.uni-hamburg.de>
2022-02-17 12:38:08 +0100

    changed name again

--- R/variable_selection_mir.R
+++ R/variable_selection_mir.R
@@ -23,7 +23,7 @@
 #' @param case.weights Weights for sampling of training observations. Observations with larger weights will be selected with higher probability in the bootstrap (or subsampled) samples for the trees.
 #' @param method.rel Method  to  compute  p-values for selection of related variables with var.relations.corr. Use  "janitza"  for  the  method  by  Janitza  et  al. (2016) or "permutation" to utilize permuted variables.
 #' @param method.sel Method  to  compute  p-values for selection of important variables. Use  "janitza"  for  the  method  by  Janitza  et  al. (2016) (can only be used when corrected variable relations are utilized) or "permutation" to utilize permuted variables.
-#' @param corr.rel set FALSE if non-corrected variable relations should be used for calculation of MRI. In this case the method "janitza" should not be used for selection of important variables
+#' @param corr.rel set FALSE if non-corrected variable relations should be used for calculation of MIR. In this case the method "janitza" should not be used for selection of important variables
 #' @param t variable to calculate threshold for non-corrected relation analysis. Default is 5.
 #'
 #'
@@ -31,7 +31,7 @@
 #' \itemize{
 #' \item info: list with results containing:
 #' \itemize{
-#' \item MRI: the calculated variable importance for each variable based on mutual impurity reduction.
+#' \item MIR: the calculated variable importance for each variable based on mutual impurity reduction.
 #' \item pvalue: the obtained p-values for each variable.
 #' \item selected: variables has been selected (1) or not (0).
 #' \item relations: a list containing the results of variable relation analysis.
@@ -49,7 +49,7 @@
 #' \donttest{
 #' # select variables (usually more trees are needed)
 #' set.seed(42)
-#' res = var.select.mri(x = SMD_example_data[,2:ncol(SMD_example_data)], y = SMD_example_data[,1],s = 10, ntree = 10)
+#' res = var.select.mir(x = SMD_example_data[,2:ncol(SMD_example_data)], y = SMD_example_data[,1],s = 10, ntree = 10)
 #' res$var
 #' }
 #'@references
@@ -59,7 +59,7 @@
 ##'   }
 #' @export
 
-var.select.mri = function(x = NULL, y = NULL, ntree = 500, type = "regression", s = NULL, mtry = NULL, min.node.size = 1,
+var.select.mir = function(x = NULL, y = NULL, ntree = 500, type = "regression", s = NULL, mtry = NULL, min.node.size = 1,
                           num.threads = NULL, status = NULL, save.ranger = FALSE,
                           save.memory = FALSE, min.var.p = 200, p.t.sel = 0.01, p.t.rel = 0.01, select.var = TRUE, select.rel = FALSE,
                           case.weights = NULL, corr.rel = TRUE, t = 5, method.rel = "janitza", method.sel = "janitza") {
@@ -150,17 +150,17 @@ var.select.mri = function(x = NULL, y = NULL, ntree = 500, type = "regression", 
 adj.agree = rel$surr.res
 diag(adj.agree) = 1
 
-  mri = colSums(adj.agree * RF$variable.importance)
+  mir = colSums(adj.agree * RF$variable.importance)
 
   if (select.var) {
     if (method.sel == "janitza") {
       if (corr.rel) {
       ## Mirrored VIMP (# This part is taken from ranger function)
-      m1 = mri[mri< 0]
-      m2 = mri[mri == 0]
+      m1 = mir[mir< 0]
+      m2 = mir[mir == 0]
       null.rel = c(m1, -m1, m2)
 
-      pval <- 1 - ranger:::numSmaller(mri, null.rel) / length(null.rel)
+      pval <- 1 - ranger:::numSmaller(mir, null.rel) / length(null.rel)
       names(pval) = allvariables
       selected = as.numeric(pval <= p.t.sel)
       names(selected) = names(pval)
@@ -223,28 +223,28 @@ diag(adj.agree) = 1
       adj.agree_perm = rel_perm$surr.res
       diag(adj.agree_perm) = 0
 
-      null.rel = unlist(lapply(1:f,calculate.mri.perm,
+      null.rel = unlist(lapply(1:f,calculate.mir.perm,
                                adj.agree_perm = adj.agree_perm,
                                air = RF$variable.importance,
                                allvariables = allvariables))
 
 
 
-      pval <- 1 - ranger:::numSmaller(mri, null.rel) / length(null.rel)
+      pval <- 1 - ranger:::numSmaller(mir, null.rel) / length(null.rel)
       names(pval) = allvariables
       selected = as.numeric(pval <= p.t.sel)
       names(selected) = names(pval)
 
       }
 
-  info = list(MRI = mri,
+  info = list(MIR = mir,
               pvalue = pval,
               selected = selected,
               relations = rel,
               AIR = RF$variable.importance,
               parameters = list(s = s, type = type, mtry = mtry, p.t.sel = p.t.sel, p.t.rel = p.t.rel, method.sel = method.sel))
   } else {
-    info = list(MRI = mri,
+    info = list(MIR = mir,
                 relations = rel,
                 AIR = RF$variable.importance,
                 parameters = list(s = s, type = type, mtry = mtry))
@@ -266,9 +266,9 @@ diag(adj.agree) = 1
 #' This is an internal function
 #'
 #' @keywords internal
-calculate.mri.perm = function(r=1, adj.agree_perm, air, allvariables) {
-mri.perm = colSums(adj.agree_perm[((r-1) * length(allvariables) + 1):(r * length(allvariables)),((r-1) * length(allvariables) + 1):(r * length(allvariables))] * air,na.rm = TRUE)
-return(mri.perm)
+calculate.mir.perm = function(r=1, adj.agree_perm, air, allvariables) {
+mir.perm = colSums(adj.agree_perm[((r-1) * length(allvariables) + 1):(r * length(allvariables)),((r-1) * length(allvariables) + 1):(r * length(allvariables))] * air,na.rm = TRUE)
+return(mir.perm)
 }
 
 
